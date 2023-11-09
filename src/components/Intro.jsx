bc8bc31269aa599f99d2cadb8bdca4509afe0b0a
HKKIM <dante8@gmail.com>
2023-11-09 11:44:28 +0900

    인트로 완료

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
+import about from "../assets/img/about.jpg";
+
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <section id="intro">
+      <div className="intro__inner">
+        <h1 className="intro__title">port developer</h1>
+        <div className="intro__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="intro__text">
+          <div className="text">
+            <div>talent is</div>
+            <div>found at the end of the</div>
+            <div>effort</div>
+          </div>
+          <div className="img">
+            <img src={about} alt="어바웃" />
+          </div>
+        </div>
+        <div className="intro__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
